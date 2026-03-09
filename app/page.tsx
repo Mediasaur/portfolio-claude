7346f95dccc3d01d92922c2f4392389d79b12adc
ErgunK <mediasaur@gmail.com>
2026-03-09 16:52:04 +0300

    test change

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Portfolio Main Page</h1>
+      <h1>Portfolio Main Page Test</h1>
     </div>
   );
 }
